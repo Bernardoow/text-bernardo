1d81ad257b088402cff74915d81301d3fda5f971
Bernardo Gomes <bgomesdeabreu@gmail.com>
2019-09-14 07:20:58 -0300

    Refactored text handle.

--- text_handler.py
+++ text_handler.py
@@ -34,26 +34,31 @@ class TextHandler:
     def _remove_stop_words(self, words):
         return [word for word in words if word not in stop_words_portuguese]
 
+    def _word_tokenize_texts(self):
+        return word_tokenize(" ".join(self.texts), language="portuguese")
+
+    def _transform_to_lower_each_word(self, words):
+        return [word.lower() for word in words]
+
     def sw_vocabulary(self):
-        words = word_tokenize(" ".join(self.texts), language="portuguese")
+        words = self._word_tokenize_texts()
         words = self._remove_stop_words(words)
-        words = [word.lower() for word in words]
+        words = self._transform_to_lower_each_word(words)
         words = set(words)
         words = words - set(string.punctuation)
         words = list(words)
-
         words.sort()
+
         return words
         # return TreebankWordTokenizer().tokenize("".join(self.texts))
 
     def sw_frequency_distribution(self):
-        words = word_tokenize(" ".join(self.texts), language="portuguese")
+        words = self._word_tokenize_texts()
         words = self._remove_stop_words(words)
-        words = [word.lower() for word in words]
+        words = self._transform_to_lower_each_word(words)
         words = [word for word in words if word not in string.punctuation]
-        words = list(words)
-
         words.sort()
+
         freq_dist = FreqDist(words)
 
         return [freq for freq in freq_dist.values()]
