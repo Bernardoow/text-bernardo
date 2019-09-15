f3a2f8d6cd05cc82bcbeefc8a879198370549e15
Bernardo Gomes <bgomesdeabreu@gmail.com>
2019-09-15 08:31:40 -0300

    Created Field To Respresent Frequency(Dict String Int) and schema to IsolatedFrequencyAPI.

--- backend/app/api_v1/schemas.py
+++ backend/app/api_v1/schemas.py
@@ -14,3 +14,40 @@ class VocabularySchema(Schema):
 
     class Meta:
         unknown = EXCLUDE
+
+
+class FrequenceDistributionField(fields.Field):
+    default_error_messages = {
+        "invalid_type": "Every value inside dict must be int.",
+        "invalid_dict": "This field must be a dict.",
+    }
+    """Field that deserializes dict string with values int
+    """
+
+    def _check_if_every_item_is_int(self, dict_string_int):
+        if isinstance(dict_string_int, dict):
+            for list_value in dict_string_int.values():
+                if isinstance(list_value, list):
+                    return all(map(lambda v: isinstance(v, int), list_value))
+                else:
+                    return False
+            return True
+        else:
+            raise self.make_error("invalid_dict")
+
+    def _check_if_every_key_is_string(self, dict_string_int):
+        return all(map(lambda key: isinstance(key, str), dict_string_int.keys()))
+
+    def _deserialize(self, value, attr, data, **kwargs):
+
+        if self._check_if_every_item_is_int(value) and self._check_if_every_key_is_string(value):
+            return value
+
+        raise self.make_error("invalid_type")
+
+
+class FrequenceDistributionSchema(Schema):
+    frequency = FrequenceDistributionField(attribute="frequency", required=True)
+
+    class Meta:
+        unknown = EXCLUDE
--- backend/app/api_v1/tests/test_schemas.py
+++ backend/app/api_v1/tests/test_schemas.py
@@ -1,7 +1,12 @@
 import pytest
-from marshmallow import Schema
+from marshmallow import Schema, fields
 from marshmallow.exceptions import ValidationError
-from app.api_v1.schemas import SendTextSchema, VocabularySchema
+from app.api_v1.schemas import (
+    FrequenceDistributionField,
+    FrequenceDistributionSchema,
+    SendTextSchema,
+    VocabularySchema,
+)
 
 
 class TestSendTextSchema:
@@ -61,3 +66,69 @@ class TestVocabularySchema:
         new_data = self.schema.load(data)
         del data["another"]
         assert data == new_data
+
+
+class TestFrequenceDistributionField:
+    @pytest.fixture(autouse=True)
+    def setUpAndTearDown(self):
+        self.field = FrequenceDistributionField()
+
+    def test_should_be_instace_of_schema(self):
+        assert isinstance(self.field, fields.Field)
+
+    def test_should_create_a_dict_empty_when_param_is_dict_empty(self):
+        assert self.field.deserialize({}) == {}
+
+    def test_should_check_if_every_value_in_dict_values_is_int(self):
+        with pytest.raises(ValidationError):
+            assert self.field.deserialize({"frequency": ["aab", "bab", "aa", "ab"]}) == {}
+
+    def test_should_check_if_every_key_is_string(self):
+        with pytest.raises(ValidationError):
+            data = {1: [1, 2, 3], "text2": [5, 6, 7]}
+            assert self.field.deserialize(data) == data
+
+    def test_should_create_dict_with_key_string_and_values_int(self):
+        data = {"text1": [1, 2, 3], "text2": [5, 6, 7]}
+        assert self.field.deserialize(data) == data
+
+
+class TestFrequenceDistributionSchemaSchema:
+    @pytest.fixture(autouse=True)
+    def setUpAndTearDown(self):
+        self.schema = FrequenceDistributionSchema()
+
+    def test_should_be_instace_of_schema(self):
+        assert isinstance(self.schema, Schema)
+
+    def test_validate_should_throw_exception_when_validate_wrong_data(self):
+        with pytest.raises(ValidationError):
+            data = {"country": "brazil"}
+            self.schema.load(data)
+
+    def test_validate_should_throw_exception_when_invalidate_data_is_in_frequency_field(self):
+        with pytest.raises(ValidationError):
+            data = {"frequency": ["aab", "bab", "aa", "ab"]}
+            new_data = self.schema.load(data)
+            assert data == new_data
+
+    def test_load_should_create_a_json_with_validate_data(self):
+        data = {"frequency": {"text1": [1, 2, 3], "text2": [5, 6, 7]}}
+        new_data = self.schema.load(data)
+        assert data == new_data
+        data = {
+            "frequency": {
+                "text1": [1, 2, 3],
+                "text2": [5, 6, 7],
+                "text3": [5, 6, 7],
+                "text4": [5, 6, 7],
+            }
+        }
+        new_data = self.schema.load(data)
+        assert data == new_data
+
+    def test_load_should_throw_a_exception_invalid_dict__invalid_values(self):
+        with pytest.raises(ValidationError):
+            data = {"frequency": {"text1": [1, "2", 3], "text2": [5, 6, 7]}}
+            new_data = self.schema.load(data)
+            assert data == new_data
