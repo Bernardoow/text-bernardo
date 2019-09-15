c7c2189cddd11eef28cb702aa64d04b75a9febe8
Bernardo Gomes <bgomesdeabreu@gmail.com>
2019-09-15 15:20:54 -0300

    Added index html.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,13 +2,16 @@
 #
 from flask import Flask, render_template
 
-from app.extensions import db, migrate
+from app.extensions import cors, db, migrate
 from app.api_v1 import api_bp_v1
 
 
 def initialize_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
+
+    if app.config["ENV"] == "development":
+        cors.init_app(app)
 
 
 def register_blueprints(app):
@@ -20,5 +23,9 @@ def create_app(config_filename=None):
     app.config.from_pyfile(config_filename)
     initialize_extensions(app)
     register_blueprints(app)
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
     return app
